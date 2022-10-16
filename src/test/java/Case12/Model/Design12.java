b222556080d0175af87e7cc0c24acde1a7646c62
bilalMavus <bilalmavuss@gmail.com>
2022-10-16 20:20:21 +0300

    Case12 finished

--- src/test/java/Case12/Model/Design12.java
+++ src/test/java/Case12/Model/Design12.java
@@ -1,4 +1,42 @@
 package Case12.Model;
 
+import java.util.Arrays;
+
 public class Design12 {
+    private String id;
+    private String name;
+    private String[] translateName;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String[] getTranslateName() {
+        return translateName;
+    }
+
+    public void setTranslateName(String[] translateName) {
+        this.translateName = translateName;
+    }
+
+    @Override
+    public String toString() {
+        return "Design12{" +
+                "id='" + id + '\'' +
+                ", name='" + name + '\'' +
+                ", translateName=" + Arrays.toString(translateName) +
+                '}';
+    }
 }
--- src/test/java/Case12/Nationalities.java
+++ src/test/java/Case12/Nationalities.java
@@ -1,6 +1,123 @@
 package Case12;
 
+import Case12.Model.Design12;
+import Tools.Login;
+import io.restassured.http.ContentType;
+import io.restassured.http.Cookies;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.equalTo;
+
 public class Nationalities {
     // Setup	Parameters	Nationalities
+    Cookies cookies;
+    @BeforeClass
+    public void login() {
+        cookies = Login.loginCampus();
+    }
+    String designId;
+    String designName;
+    @Test
+    public void createDocumentTypes() {
 
+        designName = Tools.Random.getRandomName() + " " + Tools.Random.getRandomName();
+        Design12 design12 = new Design12();
+        design12.setName(designName);
+        designId =
+                given()
+                        .cookies(cookies)
+                        .contentType(ContentType.JSON)
+                        .body(design12)
+                        .when()
+                        .post("school-service/api/nationality")
+                        .then()
+                        .log().body()
+                        .statusCode(201)
+                        .extract().jsonPath().getString("id")
+        ;
+    }
+    @Test(dependsOnMethods = "createDocumentTypes")
+    public void createDocumentTypesNegative() {
+
+        designName = Tools.Random.getRandomName() + " " + Tools.Random.getRandomName();
+        Design12 design12 = new Design12();
+        design12.setName(designName);
+        design12.setId(designId);
+        given()
+                .cookies(cookies)
+                .contentType(ContentType.JSON)
+                .body(design12)
+                .when()
+                .post("school-service/api/nationality")
+                .then()
+                .log().body()
+                .statusCode(400)
+        ;
+    }
+    @Test(dependsOnMethods = "createDocumentTypes")
+    public void updateDocumentTypes() {
+
+        designName = Tools.Random.getRandomName() + " " + Tools.Random.getRandomName();
+        Design12 design12 = new Design12();
+        design12.setName(designName);
+        design12.setId(designId);
+        given()
+                .cookies(cookies)
+                .contentType(ContentType.JSON)
+                .body(design12)
+                .when()
+                .put("school-service/api/nationality")
+                .then()
+                .log().body()
+                .statusCode(200)
+                .body("name", equalTo(designName))
+        ;
+    }
+    @Test(dependsOnMethods = "updateDocumentTypes")
+    public void deleteDocumentTypes() {
+
+        given()
+                .cookies(cookies)
+                .pathParam("designId", designId)
+                .when()
+                .delete("school-service/api/nationality/{designId}")
+                .then()
+                .log().body()
+                .statusCode(200)
+        ;
+    }
+    @Test(dependsOnMethods = "deleteDocumentTypes")
+    public void deleteDocumentTypesNegative() {
+
+        given()
+                .cookies(cookies)
+                .pathParam("designId", designId)
+                .when()
+                .delete("school-service/api/nationality/{designId}")
+                .then()
+                .log().body()
+                .statusCode(400)
+        ;
+    }
+    @Test(dependsOnMethods = "deleteDocumentTypes")
+    public void updateDocumentTypesNegative() {
+
+        designName = Tools.Random.getRandomName() + " " + Tools.Random.getRandomName();
+        Design12 design12 = new Design12();
+        design12.setName(designName);
+        design12.setId(designId);
+        given()
+                .cookies(cookies)
+                .contentType(ContentType.JSON)
+                .body(design12)
+                .when()
+                .put("school-service/api/nationality")
+                .then()
+                .log().body()
+                .statusCode(400)
+                .body("message", equalTo("Can't find Nationality"))
+        ;
+    }
 }
