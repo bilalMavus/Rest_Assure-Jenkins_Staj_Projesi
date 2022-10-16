9ca6ddec058fb0c68a3075abbbbb065d16bc6d00
bilalMavus <bilalmavuss@gmail.com>
2022-10-16 18:27:40 +0300

    Case06 finished

--- src/test/java/Case06/Model/Design06.java
+++ src/test/java/Case06/Model/Design06.java
@@ -1,4 +1,62 @@
 package Case06.Model;
 
+import java.util.Arrays;
+
 public class Design06 {
+    private boolean active;
+    private String code;
+    private String id;
+    private String name;
+    private String[] translateName;
+
+    public boolean isActive() {
+        return active;
+    }
+
+    public void setActive(boolean active) {
+        this.active = active;
+    }
+
+    public String getCode() {
+        return code;
+    }
+
+    public void setCode(String code) {
+        this.code = code;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String[] getTranslateName() {
+        return translateName;
+    }
+
+    public void setTranslateName(String[] translateName) {
+        this.translateName = translateName;
+    }
+
+    @Override
+    public String toString() {
+        return "Design06{" +
+                "active=" + active +
+                ", code='" + code + '\'' +
+                ", id='" + id + '\'' +
+                ", name='" + name + '\'' +
+                ", translateName=" + Arrays.toString(translateName) +
+                '}';
+    }
 }
--- src/test/java/Case06/SubjectCategories.java
+++ src/test/java/Case06/SubjectCategories.java
@@ -1,5 +1,127 @@
 package Case06;
 
+import Case06.Model.Design06;
+import Tools.Login;
+import io.restassured.http.ContentType;
+import io.restassured.http.Cookies;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.equalTo;
+
 public class SubjectCategories {
     //Education	Setup	Subject Categories
+    Cookies cookies;
+    @BeforeClass
+    public void login() {
+        cookies = Login.loginCampus();
+    }
+
+    String designId;
+    String designName;
+    String designCode;
+
+    @Test
+    public void createSubjectCategories() {
+        designName = Tools.Random.getRandomName();
+        designCode = Tools.Random.getRandomIntCode(3);
+        Design06 design06 = new Design06();
+        design06.setName(designName);
+        design06.setCode(designCode);
+        designId =
+                given()
+                        .cookies(cookies)
+                        .contentType(ContentType.JSON)
+                        .body(design06)
+                        .when()
+                        .post("school-service/api/subject-categories")
+                        .then()
+                        .log().body()
+                        .statusCode(201)
+                        .extract().jsonPath().getString("id")
+        ;
+    }
+    @Test(dependsOnMethods = "createSubjectCategories")
+    public void createSubjectCategoriesNegative() {
+        Design06 design06 = new Design06();
+        design06.setId(designId);
+        design06.setName(designName);
+        design06.setCode(designCode);
+        given()
+                .cookies(cookies)
+                .contentType(ContentType.JSON)
+                .body(design06)
+                .when()
+                .post("school-service/api/subject-categories")
+                .then()
+                .log().body()
+                .statusCode(400)
+                .body("message", equalTo("You can't create new subject category, because it already exists"))
+        ;
+    }
+    @Test(dependsOnMethods = "createSubjectCategories")
+    public void updateSubjectCategories() {
+        designName = Tools.Random.getRandomName();
+        designCode = Tools.Random.getRandomIntCode(3);
+        Design06 design06 = new Design06();
+        design06.setId(designId);
+        design06.setName(designName);
+        design06.setCode(designCode);
+        given()
+                .cookies(cookies)
+                .contentType(ContentType.JSON)
+                .body(design06)
+                .when()
+                .put("school-service/api/subject-categories")
+                .then()
+                .log().body()
+                .statusCode(200)
+                .body("code",equalTo(designCode))
+        ;
+    }
+    @Test(dependsOnMethods = "updateSubjectCategories")
+    public void deleteSubjectCategories() {
+        given()
+                .cookies(cookies)
+                .pathParam("designId",designId)
+                .when()
+                .delete("school-service/api/subject-categories/{designId}")
+                .then()
+                .log().body()
+                .statusCode(200)
+        ;
+    }
+    @Test(dependsOnMethods = "deleteSubjectCategories")
+    public void deleteSubjectCategoriesNegative() {
+        given()
+                .cookies(cookies)
+                .pathParam("designId",designId)
+                .when()
+                .delete("school-service/api/subject-categories/{designId}")
+                .then()
+                .log().body()
+                .statusCode(400)
+        ;
+    }
+    @Test(dependsOnMethods = "deleteSubjectCategories")
+    public void updateSubjectCategoriesNegative() {
+        designName = Tools.Random.getRandomName();
+        designCode = Tools.Random.getRandomIntCode(3);
+        Design06 design06 = new Design06();
+        design06.setId(designId);
+        design06.setName(designName);
+        design06.setCode(designCode);
+        given()
+                .cookies(cookies)
+                .contentType(ContentType.JSON)
+                .body(design06)
+                .when()
+                .put("school-service/api/subject-categories")
+                .then()
+                .log().body()
+                .statusCode(400)
+                .body("message",equalTo("Can't find Subject Category"))
+        ;
+    }
 }
